36174e37463d6688122d805cd737b3422977713e
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-05-13 15:20:20 +0800

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,589 @@
+/*
+Copyright (C): 2021-2030, The Chinese University of Hong Kong.
+*/
+
+
+//% color="#006400" weight=20 icon="\e1ec"
+namespace CUHK_JC_iCar {
+
+    const PCA9685_ADD = 0x41
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const SUBADR1 = 0x02
+    const SUBADR2 = 0x03
+    const SUBADR3 = 0x04
+
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
+
+    const PRESCALE = 0xFE
+
+    let initialized = false
+    let yahStrip: neopixel.Strip;
+
+    export enum enColor {
+
+        //% blockId="OFF" block="Off"
+        OFF = 0,
+        //% blockId="Red" block="Red"
+        Red,
+        //% blockId="Green" block="Green"
+        Green,
+        //% blockId="Blue" block="Blue"
+        Blue,
+        //% blockId="White" block="White"
+        White,
+        //% blockId="Cyan" block="Cyan"
+        Cyan,
+        //% blockId="Pinkish" block="Pinkish"
+        Pinkish,
+        //% blockId="Yellow" block="Yellow"
+        Yellow,
+
+    }
+
+    export enum enPos {
+
+        //% blockId="LeftState" block="LeftStatus"
+        LeftState = 0,
+        //% blockId="RightState" block="RightStatus"
+        RightState = 1
+    }
+
+    export enum enLineState {
+        //% blockId="White" block="WhiteLine"
+        White = 0,
+        //% blockId="Black" block="BlackLine"
+        Black = 1
+
+    }
+    
+    export enum enAvoidState {
+        //% blockId="OBSTACLE" block="Blocked"
+        OBSTACLE = 0,
+        //% blockId="NOOBSTACLE" block="Unblocked"
+        NOOBSTACLE = 1
+
+    }
+
+    
+    export enum enServo {
+        
+        S1 = 1,
+        S2,
+        S3
+    }
+    export enum CarState {
+        //% blockId="Car_Run" block="Forward"
+        Car_Run = 1,
+        //% blockId="Car_Back" block="Backward"
+        Car_Back = 2,
+        //% blockId="Car_Left" block="TurnLeft"
+        Car_Left = 3,
+        //% blockId="Car_Right" block="TurnRight"
+        Car_Right = 4,
+        //% blockId="Car_Stop" block="Stop"
+        Car_Stop = 5,
+        //% blockId="Car_SpinLeft" block="SpinLeft"
+        Car_SpinLeft = 6,
+        //% blockId="Car_SpinRight" block="SpinRight"
+        Car_SpinRight = 7
+    }
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADD, MODE1, 0x00)
+        setFreq(50);
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADD, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADD, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADD, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADD, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADD, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+        if (!initialized) {
+            initPCA9685();
+        }
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADD, buf);
+    }
+
+
+    function Car_run(speed1: number, speed2: number) {
+
+        speed1 = speed1 * 16; // map 350 to 4096
+        speed2 = speed2 * 16;
+        if (speed1 >= 4096) {
+            speed1 = 4095
+        }
+        if (speed2 >= 4096) {
+            speed2 = 4095
+        }
+
+        setPwm(12, 0, speed1);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, speed2);
+        setPwm(14, 0, 0);
+        //pins.digitalWritePin(DigitalPin.P16, 1);
+       // pins.analogWritePin(AnalogPin.P1, 1023-speed); //SpeedControl
+
+       // pins.analogWritePin(AnalogPin.P0, speed);//SpeedControl
+       // pins.digitalWritePin(DigitalPin.P8, 0);
+    }
+
+    function Car_back(speed1: number, speed2: number) {
+
+        speed1 = speed1 * 16; // map 350 to 4096
+        speed2 = speed2 * 16;
+        if (speed1 >= 4096) {
+            speed1 = 4095
+        }
+        if (speed2 >= 4096) {
+            speed2 = 4095
+        }
+        setPwm(12, 0, 0);
+        setPwm(13, 0, speed1);
+
+        setPwm(15, 0, 0);
+        setPwm(14, 0, speed2);
+
+        //pins.digitalWritePin(DigitalPin.P16, 0);
+        //pins.analogWritePin(AnalogPin.P1, speed); //SpeedControl
+
+        //pins.analogWritePin(AnalogPin.P0, 1023 - speed);//SpeedControl
+        //pins.digitalWritePin(DigitalPin.P8, 1);
+    }
+
+    function Car_left(speed1: number, speed2: number) {
+
+        speed1 = speed1 * 16; // map 350 to 4096
+        speed2 = speed2 * 16;
+        if (speed1 >= 4096) {
+            speed1 = 4095
+        }
+        if (speed2 >= 4096) {
+            speed2 = 4095
+        }
+        
+        setPwm(12, 0, speed1);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, speed2);
+        setPwm(14, 0, 0);
+
+        //pins.analogWritePin(AnalogPin.P0, speed);
+        //pins.digitalWritePin(DigitalPin.P8, 0);
+
+        //pins.digitalWritePin(DigitalPin.P16, 0);
+        //pins.digitalWritePin(DigitalPin.P1, 0);
+    }
+
+    function Car_right(speed1: number, speed2: number) {
+
+        speed1 = speed1 * 16; // map 350 to 4096
+        speed2 = speed2 * 16;
+        if (speed1 >= 4096) {
+            speed1 = 4095
+        }
+        if (speed2 >= 4096) {
+            speed2 = 4095
+        }
+        
+        setPwm(12, 0, speed1);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, speed2);
+        setPwm(14, 0, 0);
+        //pins.digitalWritePin(DigitalPin.P0, 0);
+        //pins.digitalWritePin(DigitalPin.P8, 0);
+
+        //pins.digitalWritePin(DigitalPin.P16, 1);
+       // pins.analogWritePin(AnalogPin.P1, 1023 - speed);
+    }
+
+    function Car_stop() {
+       
+        setPwm(12, 0, 0);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, 0);
+        setPwm(14, 0, 0);
+        //pins.digitalWritePin(DigitalPin.P0, 0);
+        //pins.digitalWritePin(DigitalPin.P8, 0);
+        //pins.digitalWritePin(DigitalPin.P16, 0);
+        //pins.digitalWritePin(DigitalPin.P1, 0);
+    }
+
+    function Car_spinleft(speed1: number, speed2: number) {
+
+        speed1 = speed1 * 16; // map 350 to 4096
+        speed2 = speed2 * 16;
+        if (speed1 >= 4096) {
+            speed1 = 4095
+        }
+        if (speed2 >= 4096) {
+            speed2 = 4095
+        }        
+        
+        setPwm(12, 0, 0);
+        setPwm(13, 0, speed1);
+
+        setPwm(15, 0, speed2);
+        setPwm(14, 0, 0);
+
+        //pins.analogWritePin(AnalogPin.P0, speed);
+        //pins.digitalWritePin(DigitalPin.P8, 0);
+
+        //pins.digitalWritePin(DigitalPin.P16, 0);
+        //pins.analogWritePin(AnalogPin.P1, speed);
+    } 
+
+    function Car_spinright(speed1: number, speed2: number) {
+
+        speed1 = speed1 * 16; // map 350 to 4096
+        speed2 = speed2 * 16;
+        if (speed1 >= 4096) {
+            speed1 = 4095
+        }
+        if (speed2 >= 4096) {
+            speed2 = 4095
+        }      
+        setPwm(12, 0, speed1);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, 0);
+        setPwm(14, 0, speed2);
+        //pins.analogWritePin(AnalogPin.P0, 1023-speed);
+        //pins.digitalWritePin(DigitalPin.P8, 1);
+
+        //pins.digitalWritePin(DigitalPin.P16, 1);
+        //pins.analogWritePin(AnalogPin.P1, 1023-speed);
+
+    }
+
+    /**
+     * *****************************************************************
+     * @param index
+     */
+    //% blockId=mbit_RGB_Car_Big2 block="RGB_Car_Big2|value %value"
+    //% weight=101
+    //% blockGap=10
+    //% color="#C814B8"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function RGB_Car_Big2(value: enColor): void {
+
+        switch (value) {
+            case enColor.OFF: {
+                setPwm(0, 0, 0);
+                setPwm(1, 0, 0);
+                setPwm(2, 0, 0);
+                break;
+            }
+            case enColor.Red: {
+                setPwm(0, 0, 4095);
+                setPwm(1, 0, 0);
+                setPwm(2, 0, 0);
+                break;
+            }
+            case enColor.Green: {
+                setPwm(0, 0, 0);
+                setPwm(1, 0, 4095);
+                setPwm(2, 0, 0);
+                break;
+            }
+            case enColor.Blue: {
+                setPwm(0, 0, 0);
+                setPwm(1, 0, 0);
+                setPwm(2, 0, 4095);
+                break;
+            }
+            case enColor.White: {
+                setPwm(0, 0, 4095);
+                setPwm(1, 0, 4095);
+                setPwm(2, 0, 4095);
+                break;
+            }
+            case enColor.Cyan: {
+                setPwm(0, 0, 0);
+                setPwm(1, 0, 4095);
+                setPwm(2, 0, 4095);
+                break;
+            }
+            case enColor.Pinkish: {
+                setPwm(0, 0, 4095);
+                setPwm(1, 0, 0);
+                setPwm(2, 0, 4095);
+                break;
+            }
+            case enColor.Yellow: {
+                setPwm(0, 0, 4095);
+                setPwm(1, 0, 4095);
+                setPwm(2, 0, 0);
+                break;
+            }
+        }
+    }
+    //% blockId=mbit_RGB_Car_Big block="RGB_Car_Big|value1 %value1|value2 %value2|value3 %value3"
+    //% weight=100
+    //% blockGap=10
+    //% color="#C814B8"
+    //% value1.min=0 value1.max=255 value2.min=0 value2.max=255 value3.min=0 value3.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function RGB_Car_Big(value1: number, value2: number, value3: number): void {
+
+        let R = value1 * 16;
+        let G = value2 * 16;
+        let B = value3 * 16;
+
+        if (R > 4096)
+            R = 4095;
+        if (G > 4096)
+            G = 4095;
+        if (B > 4096)
+            B = 4095;
+
+        setPwm(0, 0, R);
+        setPwm(1, 0, G);
+        setPwm(2, 0, B);
+
+    }
+
+    //% blockId=mbit_RGB_Car_Program block="RGB_Car_Program"
+    //% weight=99
+    //% blockGap=10
+    //% color="#C814B8"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function RGB_Car_Program(): neopixel.Strip {
+         
+        if (!yahStrip) {
+            yahStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB);
+        }
+        return yahStrip;  
+    }
+
+
+	//% blockId=mbit_ultrasonic_car block="ultrasonic return distance(cm)"
+    //% color="#006400"
+    //% weight=98
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Ultrasonic_Car(): number {
+
+        // send pulse   
+        let list:Array<number> = [0, 0, 0, 0, 0];
+        for (let i = 0; i < 5; i++) {
+            pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
+		        pins.digitalWritePin(DigitalPin.P14, 0);
+		        control.waitMicros(2);
+		        pins.digitalWritePin(DigitalPin.P14, 1);
+		        control.waitMicros(15);
+		        pins.digitalWritePin(DigitalPin.P14, 0);
+		
+		        let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
+		        list[i] = Math.floor(d / 40)
+        }
+        list.sort();
+        let length = (list[1] + list[2] + list[3])/3;
+        return  Math.floor(length);
+    }
+
+    //% blockId=mbit_Servo_Car block="Servo_Car|num %num|value %value"
+    //% weight=96
+    //% blockGap=10
+    //% color="#006400"
+    //% num.min=1 num.max=3 value.min=0 value.max=180
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=9
+    export function Servo_Car(num: enServo, value: number): void {
+
+        // 50hz: 20,000 us
+        let us = (value * 1800 / 180 + 600); // 0.6 ~ 2.4
+        let pwm = us * 4096 / 20000;
+        setPwm(num + 2, 0, pwm);
+
+    }
+
+    //% blockId=mbit_Avoid_Sensor block="Avoid_Sensor|value %value"
+    //% weight=95
+    //% blockGap=10
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
+    export function Avoid_Sensor(value: enAvoidState): boolean {
+
+        let temp: boolean = false;
+        pins.setPull(DigitalPin.P9, PinPullMode.PullUp)
+        pins.digitalWritePin(DigitalPin.P9, 0);
+        control.waitMicros(100);
+        switch (value) {
+            case enAvoidState.OBSTACLE: {
+                serial.writeNumber(pins.analogReadPin(AnalogPin.P3))
+                if (pins.analogReadPin(AnalogPin.P3) < 800) {
+                
+                    temp = true;
+                    setPwm(8, 0, 0);
+                }
+                else {                 
+                    temp = false;
+                    setPwm(8, 0, 4095);
+                }
+                break;
+            }
+
+            case enAvoidState.NOOBSTACLE: {
+                if (pins.analogReadPin(AnalogPin.P3) > 800) {
+
+                    temp = true;
+                    setPwm(8, 0, 4095);
+                }
+                else {
+                    temp = false;
+                    setPwm(8, 0, 0);
+                }
+                break;
+            }
+        }
+        pins.digitalWritePin(DigitalPin.P9, 1);
+        return temp;
+
+    }
+    //% blockId=mbit_Line_Sensor block="Line_Sensor|direct %direct|value %value"
+    //% weight=94
+    //% blockGap=10
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
+    export function Line_Sensor(direct: enPos, value: enLineState): boolean {
+
+        let temp: boolean = false;
+
+        switch (direct) {
+            case enPos.LeftState: {
+                if (pins.analogReadPin(AnalogPin.P2) < 500) {
+                    if (value == enLineState.White) {
+                        temp = true;
+                    }
+                    setPwm(7, 0, 4095);
+                }
+                else {
+                    if (value == enLineState.Black) {
+                        temp = true;
+                    }
+                    setPwm(7, 0, 0);
+                }
+                break;
+            }
+
+            case enPos.RightState: {
+                if (pins.analogReadPin(AnalogPin.P1) < 500) {
+                    if (value == enLineState.White) {
+                        temp = true;
+                    }
+                    setPwm(6, 0, 4095);
+                }
+                else {
+                    if (value == enLineState.Black) {
+                        temp = true;
+                    }
+                    setPwm(6, 0, 0);
+                }
+                break;
+            }
+        }
+        return temp;
+
+    }
+    //% blockId=mbit_CarCtrl block="CarCtrl|%index"
+    //% weight=93
+    //% blockGap=10
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrl(index: CarState): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(255, 255); break;
+            case CarState.Car_Back: Car_back(255, 255); break;
+            case CarState.Car_Left: Car_left(0, 255); break;
+            case CarState.Car_Right: Car_right(255, 0); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(255, 255); break;
+            case CarState.Car_SpinRight: Car_spinright(255, 255); break;
+        }
+    }
+    //% blockId=mbit_CarCtrlSpeed block="CarCtrlSpeed|%index|speed %speed"
+    //% weight=92
+    //% blockGap=10
+    //% speed.min=0 speed.max=255
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrlSpeed(index: CarState, speed: number): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(speed, speed); break;
+            case CarState.Car_Back: Car_back(speed, speed); break;
+            case CarState.Car_Left: Car_left(0, speed); break;
+            case CarState.Car_Right: Car_right(speed, 0); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(speed, speed); break;
+            case CarState.Car_SpinRight: Car_spinright(speed, speed); break;
+        }
+    }
+    //% blockId=mbit_CarCtrlSpeed2 block="CarCtrlSpeed2|%index|speed1 %speed1|speed2 %speed2"
+    //% weight=91
+    //% blockGap=10
+    //% speed1.min=0 speed1.max=255 speed2.min=0 speed2.max=255
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrlSpeed2(index: CarState, speed1: number, speed2: number): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(speed1, speed2); break;
+            case CarState.Car_Back: Car_back(speed1, speed2); break;
+            case CarState.Car_Left: Car_left(0, speed2); break;
+            case CarState.Car_Right: Car_right(speed1, 0); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(speed1, speed2); break;
+            case CarState.Car_SpinRight: Car_spinright(speed1, speed2); break;
+        }
+    }
+}
