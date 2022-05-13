7cab3199591531cb77f0e0886d5eeadcace74ba6
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-05-13 15:34:49 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 Copyright (C): 2021-2030, The Chinese University of Hong Kong.
 */
 
-//% color="#006400" weight=20 icon="\e1ec"
+//% color="#006400" weight=20
 namespace CUHK_JC_iCar {
 
     //% block="show color $color"
