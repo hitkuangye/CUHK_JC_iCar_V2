b0d6c01044c13cdc7197d21df89a6710c15d5024
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-05-18 12:16:19 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -709,9 +709,9 @@ namespace CUHK_JC_iCar {
     }
   
     //% block="When remote control button | %button | is %action"
-    //% button.fieldEditor="gridpicker"
+    /*//% button.fieldEditor="gridpicker"
     //% button.fieldOptions.columns=3
-    //% button.fieldOptions.tooltips="false"
+    //% button.fieldOptions.tooltips="false"*/
     //% group="Remote Cntrol" blockGap=10
     export function onIrButton(
       button: IrButton,
