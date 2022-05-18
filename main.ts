95bb20978000511888f9b4b36ab1e2dfd25341c2
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-05-18 11:39:00 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -75,6 +75,65 @@ namespace CUHK_JC_iCar {
         //% blockId="NOOBSTACLE" block="Unblocked"
         NOOBSTACLE = 1
     }
+    const enum IrButton {
+    //% block="1"
+    Number_1 = 0xA2,
+    //% block="2"
+    Number_2 = 0x62,
+    //% block="3"
+    Number_3 = 0xE2,
+    //% block="4"
+    Number_4 = 0x22,
+    //% block="5"
+    Number_5 = 0x02,
+    //% block="6"
+    Number_6 = 0xC2,
+    //% block="7"
+    Number_7 = 0xE0,
+    //% block="8"
+    Number_8 = 0xA8,
+    //% block="9"
+    Number_9 = 0x90,
+    //% block="*"
+    Star = 0x68,
+    //% block="0"
+    Number_0 = 0x98,
+    //% block="#"
+    Hash = 0xB0,
+    //% block="any"
+    Any = -1,
+    //% block="▲"
+    Up = 0x18,
+    //% block=" "
+    Unused_2 = -2,
+    //% block="◀"
+    Left = 0x10,
+    //% block="OK"
+    Ok = 0x38,
+    //% block="▶"
+    Right = 0x90,
+    //% block=" "
+    Unused_3 = -3,
+    //% block="▼"
+    Down = 0x4A,
+    //% block=" "
+    Unused_4 = -4,
+    
+  }
+  
+  const enum IrButtonAction {
+    //% block="pressed"
+    Pressed = 0,
+    //% block="released"
+    Released = 1,
+  }
+  
+  const enum IrProtocol {
+    //% block="Keyestudio"
+    Keyestudio = 0,
+    //% block="NEC"
+    NEC = 1,
+  }
     function i2cwrite(addr: number, reg: number, value: number) {
         let buf = pins.createBuffer(2)
         buf[0] = reg
@@ -443,7 +502,292 @@ namespace CUHK_JC_iCar {
  /*****************************************************************************************************************************************
  *  Remote Control *****************************************************************************************************************************
  ****************************************************************************************************************************************/
- 
+    let irState: IrState;
+  
+    const IR_REPEAT = 256;
+    const IR_INCOMPLETE = 257;
+    const IR_DATAGRAM = 258;
+  
+    const REPEAT_TIMEOUT_MS = 120;
+  
+    interface IrState {
+      protocol: IrProtocol;
+      hasNewDatagram: boolean;
+      bitsReceived: uint8;
+      addressSectionBits: uint16;
+      commandSectionBits: uint16;
+      hiword: uint16;
+      loword: uint16;
+      activeCommand: number;
+      repeatTimeout: number;
+      onIrButtonPressed: IrButtonHandler[];
+      onIrButtonReleased: IrButtonHandler[];
+      onIrDatagram: () => void;
+    }
+    class IrButtonHandler {
+      irButton: IrButton;
+      onEvent: () => void;
+  
+      constructor(
+        irButton: IrButton,
+        onEvent: () => void
+      ) {
+        this.irButton = irButton;
+        this.onEvent = onEvent;
+      }
+    }
+  
+  
+    function appendBitToDatagram(bit: number): number {
+      irState.bitsReceived += 1;
+  
+      if (irState.bitsReceived <= 8) {
+        irState.hiword = (irState.hiword << 1) + bit;
+        if (irState.protocol === IrProtocol.Keyestudio && bit === 1) {
+          // recover from missing message bits at the beginning
+          // Keyestudio address is 0 and thus missing bits can be detected
+          // by checking for the first inverse address bit (which is a 1)
+          irState.bitsReceived = 9;
+          irState.hiword = 1;
+        }
+      } else if (irState.bitsReceived <= 16) {
+        irState.hiword = (irState.hiword << 1) + bit;
+      } else if (irState.bitsReceived <= 32) {
+        irState.loword = (irState.loword << 1) + bit;
+      }
+  
+      if (irState.bitsReceived === 32) {
+        irState.addressSectionBits = irState.hiword & 0xffff;
+        irState.commandSectionBits = irState.loword & 0xffff;
+        return IR_DATAGRAM;
+      } else {
+        return IR_INCOMPLETE;
+      }
+    }
+  
+    function decode(markAndSpace: number): number {
+      if (markAndSpace < 1600) {
+        // low bit
+        return appendBitToDatagram(0);
+      } else if (markAndSpace < 2700) {
+        // high bit
+        return appendBitToDatagram(1);
+      }
+  
+      irState.bitsReceived = 0;
+  
+      if (markAndSpace < 12500) {
+        // Repeat detected
+        return IR_REPEAT;
+      } else if (markAndSpace < 14500) {
+        // Start detected
+        return IR_INCOMPLETE;
+      } else {
+        return IR_INCOMPLETE;
+      }
+    }
+  
+    function enableIrMarkSpaceDetection(pin: DigitalPin) {
+      pins.setPull(pin, PinPullMode.PullNone);
+  
+      let mark = 0;
+      let space = 0;
+  
+      pins.onPulsed(pin, PulseValue.Low, () => {
+        // HIGH, see https://github.com/microsoft/pxt-microbit/issues/1416
+        mark = pins.pulseDuration();
+      });
+  
+      pins.onPulsed(pin, PulseValue.High, () => {
+        // LOW
+        space = pins.pulseDuration();
+        const status = decode(mark + space);
+  
+        if (status !== IR_INCOMPLETE) {
+          handleIrEvent(status);
+        }
+      });
+    }
+  
+    function handleIrEvent(irEvent: number) {
+  
+      // Refresh repeat timer
+      if (irEvent === IR_DATAGRAM || irEvent === IR_REPEAT) {
+        irState.repeatTimeout = input.runningTime() + REPEAT_TIMEOUT_MS;
+      }
+  
+      if (irEvent === IR_DATAGRAM) {
+        irState.hasNewDatagram = true;
+  
+        if (irState.onIrDatagram) {
+          background.schedule(irState.onIrDatagram, background.Thread.UserCallback, background.Mode.Once, 0);
+        }
+  
+        const newCommand = irState.commandSectionBits >> 8;
+  
+        // Process a new command
+        if (newCommand !== irState.activeCommand) {
+  
+          if (irState.activeCommand >= 0) {
+            const releasedHandler = irState.onIrButtonReleased.find(h => h.irButton === irState.activeCommand || IrButton.Any === h.irButton);
+            if (releasedHandler) {
+              background.schedule(releasedHandler.onEvent, background.Thread.UserCallback, background.Mode.Once, 0);
+            }
+          }
+  
+          const pressedHandler = irState.onIrButtonPressed.find(h => h.irButton === newCommand || IrButton.Any === h.irButton);
+          if (pressedHandler) {
+            background.schedule(pressedHandler.onEvent, background.Thread.UserCallback, background.Mode.Once, 0);
+          }
+  
+          irState.activeCommand = newCommand;
+        }
+      }
+    }
+  
+    function initIrState() {
+      if (irState) {
+        return;
+      }
+  
+      irState = {
+        protocol: undefined,
+        bitsReceived: 0,
+        hasNewDatagram: false,
+        addressSectionBits: 0,
+        commandSectionBits: 0,
+        hiword: 0, // TODO replace with uint32
+        loword: 0,
+        activeCommand: -1,
+        repeatTimeout: 0,
+        onIrButtonPressed: [],
+        onIrButtonReleased: [],
+        onIrDatagram: undefined,
+      };
+    }
+  
+    /**
+     * Connects to the IR receiver module at the specified pin and configures the IR protocol.
+     */
+    //% blockId="makerbit_infrared_connect_receiver"
+    //% block="Remote control receiver enable"
+    //% group="Remote Cntrol" blockGap=10
+    //% weight=90
+    export function connectIrReceiver(): void {
+        let protocol = 0
+        let pin = DigitalPin.P8
+
+        initIrState();
+  
+      if (irState.protocol) {
+        return;
+      }
+  
+      irState.protocol = protocol;
+  
+      enableIrMarkSpaceDetection(pin);
+  
+      background.schedule(notifyIrEvents, background.Thread.Priority, background.Mode.Repeat, REPEAT_TIMEOUT_MS);
+    }
+  
+    function notifyIrEvents() {
+      if (irState.activeCommand === -1) {
+        // skip to save CPU cylces
+      } else {
+        const now = input.runningTime();
+        if (now > irState.repeatTimeout) {
+          // repeat timed out
+  
+          const handler = irState.onIrButtonReleased.find(h => h.irButton === irState.activeCommand || IrButton.Any === h.irButton);
+          if (handler) {
+            background.schedule(handler.onEvent, background.Thread.UserCallback, background.Mode.Once, 0);
+          }
+  
+          irState.bitsReceived = 0;
+          irState.activeCommand = -1;
+        }
+      }
+    }
+  
+    /**
+     * Do something when a specific button is pressed or released on the remote control.
+     * @param button the button to be checked
+     * @param action the trigger action
+     * @param handler body code to run when the event is raised
+     */
+    //% blockId=makerbit_infrared_on_ir_button
+    //% block="When remote control button | %button | is %action"
+    //% button.fieldEditor="gridpicker"
+    //% button.fieldOptions.columns=3
+    //% button.fieldOptions.tooltips="false"
+    //% weight=50
+    //% group="Remote Cntrol" blockGap=10
+    export function onIrButton(
+      button: IrButton,
+      action: IrButtonAction,
+      handler: () => void
+    ) {
+      initIrState();
+      if (action === IrButtonAction.Pressed) {
+        irState.onIrButtonPressed.push(new IrButtonHandler(button, handler));
+      }
+      else {
+        irState.onIrButtonReleased.push(new IrButtonHandler(button, handler));
+      }
+    }
+  
+    export function irButton(): number {
+      basic.pause(0); // Yield to support background processing when called in tight loops
+      if (!irState) {
+        return IrButton.Any;
+      }
+      return irState.commandSectionBits >> 8;
+    }
+  
+
+    export function onIrDatagram(handler: () => void) {
+      initIrState();
+      irState.onIrDatagram = handler;
+    }
+    export function irDatagram(): string {
+      basic.pause(0); // Yield to support background processing when called in tight loops
+      initIrState();
+      return (
+        "0x" +
+        ir_rec_to16BitHex(irState.addressSectionBits) +
+        ir_rec_to16BitHex(irState.commandSectionBits)
+      );
+    }
+
+    export function wasIrDataReceived(): boolean {
+      basic.pause(0); // Yield to support background processing when called in tight loops
+      initIrState();
+      if (irState.hasNewDatagram) {
+        irState.hasNewDatagram = false;
+        return true;
+      } else {
+        return false;
+      }
+    }
+  
+    export function irButtonCode(button: IrButton): number {
+      basic.pause(0); // Yield to support background processing when called in tight loops
+      return button as number;
+    }
+  
+    function ir_rec_to16BitHex(value: number): string {
+      let hex = "";
+      for (let pos = 0; pos < 4; pos++) {
+        let remainder = value % 16;
+        if (remainder < 10) {
+          hex = remainder.toString() + hex;
+        } else {
+          hex = String.fromCharCode(55 + remainder) + hex;
+        }
+        value = Math.idiv(value, 16);
+      }
+      return hex;
+    }
  /*****************************************************************************************************************************************
  *  Obstacle Sensor *****************************************************************************************************************************
  ****************************************************************************************************************************************/    	    
