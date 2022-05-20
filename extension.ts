8173ee12cbe584df18594c4717a0313b132258d6
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-05-20 09:43:49 +0800

    Update extension.ts

--- extension.ts
+++ extension.ts
@@ -1,9 +1,9 @@
-//% color="#EABD00" weight=5 icon="\uf0d6" block="CUHK-JC iCar Extension"
+//% color="#EABD00" weight=5 icon="\uf085" block="CUHK-JC iCar Extension"
 
 namespace extension{ 
 
     /**
-     * This is a statement block f085
+     * This is a statement block 
      */
     //% block = "This is a testing block"
     export function test() {
