f42fd8ba137c3573c945efacdeb005f7af485e38
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-05-20 09:27:01 +0800

    Update extension.ts

--- extension.ts
+++ extension.ts
@@ -1,3 +1,11 @@
-//% weight=25  color=#F6C700 icon="\uf4c0"  block="CUHK-JC iCar Extension"
-namespace extension {
+//% color="#F6C700" weight=5 icon="\uf4c0" block="CUHK-JC iCar Extension"
+
+namespace extension{ 
+
+    /**
+     * This is a statement block
+     */
+    //% block = "This is a testing block"
+    export function test() {
+    }
 }
