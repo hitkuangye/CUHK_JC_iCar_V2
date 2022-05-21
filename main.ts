92373a75c648915c508c9f34df7c6620f0fb09cf
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-05-21 23:16:17 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ Copyright (C): 2021-2030, The Chinese University of Hong Kong.
 //% groups='["Move","Headlights","RGB Module","Ultrasonic Sensor","Line Detector","Remote Control","Obstacle Sensor","Pins"]'
 
 namespace CUHK_JC_iCar{ 
-  //declare var remote : any;
+  declare var remote : any;
   //let remote = remote.IrButtonHandler()
   const PCA9685_ADD = 0x41
   const MODE1 = 0x00
